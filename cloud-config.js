a5105ada9bdd7c785ca1485844f1b4a82426b2bb
ankitsinghsinghel <singhelboyankit@gmail.com>
2024-08-10 00:02:50 +0530

    env

--- cloud-config.js
+++ cloud-config.js
@@ -4,19 +4,20 @@ const multer=require('multer');2
 const storage=multer.memoryStorage();
 const upload=multer({storage:storage})
 
-cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret:process.env.API_SECRET
-});
+// cloudinary.config({
+//     // cloud_name:process.env.CLOUD_NAME,
+//     // api_key: process.env.API_KEY,
+//     // api_secret:process.env.API_SECRET
+// });
 
-const storage2 = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'BagtoKaban_dev',
-      allowedFormats: ['png','jpg','jpeg'],
-    },
-  });
+// const storage2 = new CloudinaryStorage({
+//     cloudinary: cloudinary,
+//     params: {
+//       folder: 'BagtoKaban_dev',
+//       allowedFormats: ['png','jpg','jpeg'],
+//     },
+//   });
+// console.log('hii');
 
   // module.exports={
   //   cloudinary,
