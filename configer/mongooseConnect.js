d1d2faa08d2cf343d80f158960c4aeea8ca90ad7
ankitsinghsinghel <singhelboyankit@gmail.com>
2024-08-10 10:09:08 +0530

    removed config

--- configer/mongooseConnect.js
+++ configer/mongooseConnect.js
@@ -1,16 +1,15 @@
-require('dotenv').config();
-const mongoose=require('mongoose');
-//using config instead of .env as it is much secure
-// const dbgr=require('debug')('development:mongoose');
+// const mongoose=require('mongoose');
+// //using config instead of .env as it is much secure
+// // const dbgr=require('debug')('development:mongoose');
 
 
-const main=async()=>{
+// const main=async()=>{
 
-    await  mongoose.connect("mongodb+srv://singhelboyankit:J5ZR9LKOzjFNePqR@bagtokabandb.nxmlo.mongodb.net/?retryWrites=true&w=majority&appName=BagtoKabandb")
-  }
-  main().then((res)=>{
-      dbgr('connected');
-  }).catch((err)=>{
-    dbgr('not connected',err);
-  })
+//     await  mongoose.connect("mongodb+srv://singhelboyankit:J5ZR9LKOzjFNePqR@bagtokabandb.nxmlo.mongodb.net/?retryWrites=true&w=majority&appName=BagtoKabandb")
+//   }
+//   main().then((res)=>{
+//       dbgr('connected');
+//   }).catch((err)=>{
+//     dbgr('not connected',err);
+//   })
       // module.exports=mongoose.connnection;  //exports the connection
